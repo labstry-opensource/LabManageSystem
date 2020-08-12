a515cc2916dd8c8a77d3c95811e640b41dd56866
Raynold <raynold_chow@hotmail.com>
2020-08-12 22:30:48 +0800

    Added Router Function

--- index.php
+++ index.php
@@ -1,14 +1,25 @@
 <?php
 
-if(defined('ROOT_DIR') || define('ROOT_DIR', dirname(__FILE__)));
-if(defined('BASE_URL') || define('BASE_URL', str_replace($_SERVER['DOCUMENT_ROOT'], '', ROOT_DIR)));
+defined('ROOT_DIR') || define('ROOT_DIR', dirname(__FILE__));
+defined('DIR') || define('DIR', dirname(__DIR__));
+defined('BASE_PATH') || define('BASE_PATH', str_replace(DIR, '', ROOT_DIR));
+defined('APP_PATH') || define('APP_PATH', $_SERVER['REQUEST_URI']);
+
+include_once dirname(__FILE__) . '/src/Router.php';
+
+$router = new Router();
 
 function get_campaign_template($campaign_name = null){
     return dirname(__FILE__ . '/campaign/' . (!empty($campaign_name) ? $campaign_name : ''));
 }
 
-$request_uri = $_SERVER['REQUEST_URI'];
-echo $_SERVER['DOCUMENT_ROOT'];
+
+$router->route(BASE_PATH . '/()/', function () {
+    header('Location: '. BASE_PATH .'/en/');
+    exit;
+});
+
+Router::execute(explode('?', $_SERVER['REQUEST_URI'])[0]);
 
 if(file_exists(get_campaign_template($page) . '/index.php')){
     include get_campaign_template($page) . '/index.php';
