b752b735e9279f136970852f55afa9a911499d99
Raynold <raynold_chow@hotmail.com>
2020-08-25 22:40:12 +0800

    Added - Let program to scan the url and the available locale to include pages respectively

--- index.php
+++ index.php
@@ -5,24 +5,58 @@ defined('DIR') || define('DIR', dirname(__DIR__));
 defined('BASE_PATH') || define('BASE_PATH', str_replace(DIR, '', ROOT_DIR));
 defined('APP_PATH') || define('APP_PATH', $_SERVER['REQUEST_URI']);
 
-include_once dirname(__FILE__) . '/src/Router.php';
+include_once ROOT_DIR . '/src/Router.php';
+
+/*
+ * Note that you should have page struct like this so that my router can handle the redirect.
+ * /page
+ * ----/en
+ * --------/intro-item-1
+ * --------/intro-2
+ * --------/....
+ * ----/zh-hk
+ * ----/.....
+*/
+
+/* We scan all language subdirectories under 'page' and setup our regex.
+ * We are doing so to allow it to redirect when new language is created.
+*/
+
+$avail_langs = array_map('basename', glob(ROOT_DIR . '/page/*', GLOB_ONLYDIR));
+
+
+$lang_join = '(' . implode('|', $avail_langs) . ')';
 
 $router = new Router();
 
-function get_campaign_template($campaign_name = null){
-    return dirname(__FILE__ ) . '/page/' . (!empty($campaign_name) ? $campaign_name : '') . '/index.php';
+function get_campaign_template($language = 'en', $campaign_name = null){
+    return dirname(__FILE__ ) . '/page/' . $language. '/'. (!empty($campaign_name) ? $campaign_name : '') . '/index.php';
 }
 
-
-$router->route(BASE_PATH . '/', function () {
-    header('Location: '. BASE_PATH .'/en/');
+$router->route(BASE_PATH . '/([\w-]+?)', function($path){
+    //Add forward slash
+    header('Location: '. BASE_PATH .'/' . $path . '/');
     exit;
 });
 
+$router->route(BASE_PATH . '/', function ($path) {
+    header('Location: '. BASE_PATH .'/en/');
+});
+
+
+/* We originally set up this path by listing all locales available.
+ * i.e. (/en|zh-hk|zh-tw....)/
+ * BUT MAN, we are lazy. Instead, we use our imploded locale scanning string to create a regex and
+ * let it handles the rest.
+ */
+
+$router->route(BASE_PATH . "/" . $lang_join . "/", function($language){
+    include ROOT_DIR . "/page/{$language}/home.php";
+});
 
 $router->route(BASE_PATH . '/(en|zh-hk)/([\w-]+?)/', function ($language, $page){
-    if(file_exists(get_campaign_template($page))){
-        include get_campaign_template($page);
+    if(file_exists(get_campaign_template($language, $page))){
+        include get_campaign_template($language, $page);
     }
 });
 
