7193e5c4cfc25fdc63dc8650b44e1adcb6cc76e7
Raynold <raynold_chow@hotmail.com>
2020-09-03 21:08:15 +0800

    Added Meedo plugin

--- admin/login.php
+++ admin/login.php
@@ -10,9 +10,32 @@ include_once dirname(__FILE__) . '/admin-header.php';
 
 <div class="h-100 d-flex flex-column bg-pop-mosaic">
     <div class="d-flex flex-fill justify-content-center align-items-center">
-        <div class="login-wrapper">
+        <div class="login-wrapper d-flex flex-column text-light">
             <img class="svg-inline m-auto login-logo" src="<?php echo BASE_PATH . '/assets/lms-logo.svg'?>" alt="">
             <form action="">
+                <div class="p-2 my-3 bg-light text-pop-mosaic">
+                    You must login before continue.
+                </div>
+                <div class="row align-items-center">
+                    <div class="col-auto">
+                        <div class="form-group mb-3">
+                            <!-- BS Compatible -->
+                            <label for="username">Username</label>
+                            <input type="text" id="username" class="form-control" name="username">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" name="password">
+                        </div>
+                    </div>
+                    <div class="col-auto">
+                        <button type="submit" class="btn btn-outline-light w-100">
+                            Login
+                            <img class="svg-inline" src="<?php echo BASE_PATH . '/assets/admin/arrow-login.svg'?>" alt="">
+                        </button>
+                    </div>
+                </div>
+
             </form>
         </div>
     </div>
