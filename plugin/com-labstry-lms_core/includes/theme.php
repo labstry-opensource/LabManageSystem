06d434519958e1b9289b695b7562a1049e468123
littlephone <raynold_chow@hotmail.com>
2020-11-14 23:41:37 +0000

    Added documentation of the new added functions getModule and theModule

--- plugin/com-labstry-lms_core/includes/theme.php
+++ plugin/com-labstry-lms_core/includes/theme.php
@@ -10,10 +10,14 @@ function getActiveThemeUrl(){
 }
 
 function getModule($module_name){
+    /* Return the module path that caller function requests.
+     * Note: This function don't guarantee that the module exits
+     * */
     return getActiveThemeDir() . 'modules/' . $module_name . '.php';
 }
 
 function theModule($module_name){
+    /* Include the moudule if exists inside modules dir of ACTIVE THEME */
     if(file_exists(getModule($module_name))){
         include getModule($module_name);
     }
